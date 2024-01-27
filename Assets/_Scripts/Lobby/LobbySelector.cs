9e41fc032c1a000f11665eb9fd9daca85b7640d0
Carlijavier5 <carlijavier5@gmail.com>
2024-01-27 15:53:52 -0500

    Network Stuff

---
+++ Assets/_Scripts/Lobby/LobbySelector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LobbySelector : MonoBehaviour {
+
+    [SerializeField] private float amplitude;
+    private Vector3 startPos;
+
+    void Awake() {
+        startPos = transform.position;
+    }
+
+    void Update() {
+        transform.position = new Vector3(startPos.x, startPos.y + Mathf.Sin(Time.time) * amplitude, startPos.z);
+    }
+}
---
+++ Assets/_Scripts/Lobby/PlayerSpawn.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerSpawn : NetworkBehaviour {
+    public event System.Action<bool> OnSpawnToggle;
+
+    [SerializeField] private GameObject playerPrefab;
+    //[SerializeField] private ColorPanel colorPanel;
+    public NetworkObject PlayerNO { get; private set; }
+    private bool isHost;
+
+    public override void OnNetworkSpawn() {
+        NetworkObject.CheckObjectVisibility = (clientID) => true;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void AdmitServerRPC(ulong clientID) {
+        GameObject playerGO = Instantiate(playerPrefab, transform.position, transform.rotation);
+        PlayerNO = playerGO.GetComponent<NetworkObject>();
+        PlayerNO.Spawn(true);
+        PlayerNO.ChangeOwnership(clientID);
+    }
+
+    public void Init() => OnSpawnToggle?.Invoke(true);
+
+    public void Dispose() {
+        Destroy(PlayerNO);
+        NetworkObject.RemoveOwnership();
+        OnSpawnToggle?.Invoke(false);
+    }
+}
