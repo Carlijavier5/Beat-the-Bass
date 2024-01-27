0be4294aff2817a898ddc296b0a47e9a6298147b
chrysly <chrysly0816@gmail.com>
2024-01-27 15:29:18 -0500

    Player controller script

--- Assets/_Scripts/Entities/EntityData.cs
+++ Assets/_Scripts/Entities/EntityData.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Entity Data/Base Entity")]
 public class EntityData : ScriptableObject
 {
-    [Header("Entity Details")]
+    [Header("Entity Attributes")]
     public string displayName = "";
 
     [Range(0f, 3f)] public float weight;
