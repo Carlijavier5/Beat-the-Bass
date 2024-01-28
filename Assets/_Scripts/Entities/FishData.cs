3c476ad31c363a9d9f9a4b3a0be0e7e070e8b412
Ivu <ivucreates@gmail.com>
2024-01-28 03:41:44 -0500

    fish flopy

--- Assets/_Scripts/Entities/FishData.cs
+++ Assets/_Scripts/Entities/FishData.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Entity Data/Fish Entity")]
 public class FishData : EntityData
 {
-    
+    public float moveInterval = 1f;
+    public float flopMagnitude = 5f;
 }
--- Assets/_Scripts/Entities/FishEntity.cs
+++ Assets/_Scripts/Entities/FishEntity.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FishEntity : Entity
 {
     private FishData fishData;
+    private float timer = 0.0f;
 
     protected override void Awake() {
         base.Awake();
@@ -15,7 +16,22 @@ public class FishEntity : Entity
         base.FixedUpdate();
     }
 
-    private void MoveFish() {
+    void Update() {
+        timer += Time.deltaTime;
 
+        if (timer >= fishData.moveInterval) {
+            MoveFish();
+            timer = 0.0f;
+        }
     }
+
+    private void MoveFish() {
+        Vector3 randDirection = Random.onUnitSphere;
+        randDirection.y = 0;
+
+        Vector3 localDirection = transform.TransformDirection(randDirection);
+        rigidbody.AddRelativeForce(localDirection * fishData.flopMagnitude, ForceMode.Impulse);
+    }
+
+    // get direction between two points
 }
