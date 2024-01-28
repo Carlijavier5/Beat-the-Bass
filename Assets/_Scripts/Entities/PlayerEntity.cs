0d6ee7a1d662da7e880b30da686c697eb963eff3
chrysly <chrysly0816@gmail.com>
2024-01-28 12:35:34 -0500

    Movement lock

--- Assets/_Scripts/Entities/PlayerEntity.cs
+++ Assets/_Scripts/Entities/PlayerEntity.cs
@@ -6,9 +6,14 @@ using UnityEngine;
 public class PlayerEntity : Entity {
 
     private PlayerData playerData;
+    [SerializeField] private GameObject hitCollider;
+    [SerializeField] private float hitDelay = 1f;
+    private IEnumerator activeHit = null;
+    private bool canMove = true;
     void Awake() {
         playerData = (PlayerData) Data;
         rb = GetComponent<Rigidbody>();
+        GameManager.Instance.Input.OnBeat += HandleAttack;
         //GameManager.Instance.Input.OnMove += HandleInput;
     }
 
@@ -16,11 +21,27 @@ public class PlayerEntity : Entity {
         HandleInput();
     }
 
+    public void CanMove(bool enableMove) {
+        this.canMove = enableMove;
+    }
+
     private void HandleInput() {
         if (!IsOwner) return;
         Vector2 input = GameManager.Instance.Input.MoveVector;
         Vector3 direction = new Vector3(input.x, 0f, input.y) * playerData.moveSpeed;
         Debug.Log(direction);
-        rb.AddRelativeForce(direction);
+        if (canMove) rb.AddRelativeForce(direction);
+    }
+
+    private void HandleAttack() {
+        if (activeHit == null) {
+            activeHit = AttackAction();
+            StartCoroutine(activeHit);
+        }
+    }
+
+    private IEnumerator AttackAction() {
+        GameObject attack = Instantiate(hitCollider, transform.position + transform.forward * 2, transform.rotation);
+        Destroy(attack, 0.5f);
     }
 }
