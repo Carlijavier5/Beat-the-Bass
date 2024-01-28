3b714cd180a27c6b98dde1c446d71f72c9f91ccb
Ivu <ivucreates@gmail.com>
2024-01-28 03:56:10 -0500

    Update FishTimer.cs

--- Assets/_Scripts/Entities/FishTimer.cs
+++ Assets/_Scripts/Entities/FishTimer.cs
@@ -1,3 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FishTimer : MonoBehaviour
+{
+    [SerializeField] private Image uiFill;
+    [SerializeField] private Text uiText;
+
     private FishEntity fish;
     private float flopTime;
 
