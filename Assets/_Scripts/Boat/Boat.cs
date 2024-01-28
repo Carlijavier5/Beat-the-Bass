d446990530c55c09f409752c715e1bf0dd3d9de2
Ivu <ivucreates@gmail.com>
2024-01-27 21:28:29 -0500

    in progress boat lerping

--- Assets/_Scripts/Boat/Boat.cs
+++ Assets/_Scripts/Boat/Boat.cs
@@ -8,13 +8,16 @@ public class Boat : MonoBehaviour {
     public float getDragInstance() { return dragInstance; }
 
     private float rotationSpeed = 100f;
-    public Vector3 currentRotation;
+    public Vector3 targetRotation;
     private Vector3 originalEulers;
 
     private float clampRotationAroundZ = 30f;
     private float clampRotationAroundX = 30f;
 
     private List<Entity> objectsOnBoat = new();
+
+    public AnimationCurve moveCurve;
+    private float animationTimePosition;
 
     void Start() {
         originalEulers = transform.eulerAngles;
@@ -45,37 +48,49 @@ public class Boat : MonoBehaviour {
             // calc the avg X-Z position of all children
             float totalPositionX = 0f;
             float totalPositionZ = 0f;
-            float testWeight = 1f;
             float individualWeightX = 0f;
             float individualWeightZ = 0f;
             for (int i = 0; i < objectsOnBoat.Count; ++i) {
                 Vector3 objectPosition = objectsOnBoat[i].transform.localPosition; 
                 //calc individual weights (how much to rotate)
-                individualWeightX = objectPosition.x * testWeight;
-                individualWeightZ = objectPosition.z * testWeight;
+                individualWeightX = objectPosition.x * objectsOnBoat[i].data.weight;
+                //individualWeightZ = objectPosition.z * objectsOnBoat[i].data.weight;
 
                 totalPositionX += individualWeightX;
-                totalPositionZ += individualWeightZ;
+                //totalPositionZ += individualWeightZ;
             }
-            currentRotation = currentRotation + new Vector3(totalPositionZ, 0f, -totalPositionX) * Time.deltaTime * rotationSpeed;
-            currentRotation = new Vector3(currentRotation.x % 360, currentRotation.y % 360, currentRotation.z % 360);
+            targetRotation = targetRotation + new Vector3(totalPositionZ, 0f, -totalPositionX) * Time.deltaTime * rotationSpeed;    //
+            targetRotation = new Vector3(targetRotation.x % 360, targetRotation.y % 360, targetRotation.z % 360);
 
             //overall rotation
-            currentRotation.z = Mathf.Clamp(currentRotation.z, -clampRotationAroundZ, clampRotationAroundZ);    // rotation around the Z is based on x coords and vice versa
-            currentRotation.x = Mathf.Clamp(currentRotation.x, -clampRotationAroundZ, clampRotationAroundZ);
+            targetRotation.z = Mathf.Clamp(targetRotation.z, -clampRotationAroundZ, clampRotationAroundZ);    // rotation around the Z is based on x coords and vice versa
+           // targetRotation.x = Mathf.Clamp(targetRotation.x, -clampRotationAroundZ, clampRotationAroundZ);
 
             // if the tilt value is within a certain range then don't keep rotating the boat
-            float currentZTilt = CalculateBoatTilt(currentRotation.z);
-            //Debug.Log(currentZTilt);
+            float currentZTilt = CalculateBoatTilt(targetRotation.z);
             if (currentZTilt >= -0.1f && currentZTilt <= 0.1f) {
                 // keep boat steady
                 Debug.Log("steady");
-                transform.eulerAngles = originalEulers;
-                currentRotation = new Vector3(0f, 0f, 0f);
+                transform.eulerAngles = originalEulers; //
+                transform.eulerAngles = Vector3.MoveTowards(transform.eulerAngles, originalEulers, Time.deltaTime);
+                //targetRotation = new Vector3(0f, 0f, 0f);   // movetowards transform
             }
             else {
-                transform.eulerAngles = currentRotation;
+
+                //transform.eulerAngles = targetRotation;
+                //RotateBetweenPoints(transform.eulerAngles, targetRotation);
             }
+        }
+    }
+
+    private void RotateBetweenPoints(Vector3 startRot, Vector3 targetRot) {
+        if (startRot != targetRot) {
+            animationTimePosition += Time.deltaTime;
+            float angle = Mathf.LerpAngle(startRot.x, targetRot.x, moveCurve.Evaluate(animationTimePosition));
+            Debug.Log(angle);
+            transform.eulerAngles = new Vector3(0f, 0f, angle);
+        } else {
+            animationTimePosition = 0f;
         }
     }
 
@@ -83,7 +98,6 @@ public class Boat : MonoBehaviour {
         float normalizedTilt = currentRotAxis / clampRotationAroundZ;
         return Mathf.Clamp(normalizedTilt, -1f, 1f);
     }
-    // lerp animation curve
 
     public Vector3 GetCurrentEulers() {
         return transform.eulerAngles;
